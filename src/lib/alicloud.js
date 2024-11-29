4d62d78ae9c017060e51c7df16a48e287424e50d
XFQ123 <3110089896@qq.com>
2024-11-29 13:49:01 +0800

    "避免环境变量冲突"

--- src/lib/alicloud.js
+++ src/lib/alicloud.js
@@ -6,16 +6,4 @@ const ossConfig = {
     bucket: process.env.ALI_OSS_BUCKET
 };
 
-const dbConfig = {
-    // MongoDB配置
-    mongoUrl: process.env.MONGODB_URL,
-    // 或 MySQL配置
-    mysql: {
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
-    }
-};
-
-export { ossConfig, dbConfig };
+export { ossConfig };
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,5 @@
 import mysql from 'mysql2/promise';
-import { dbConfig } from './alicloud';
+
 
 
 const getConfig = () => {
